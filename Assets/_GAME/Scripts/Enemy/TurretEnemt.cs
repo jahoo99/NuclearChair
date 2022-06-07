ece12f92c92d5dd8d6e9d13b1448b0839db2a464
Jan <jspiewak@edu.cdv.pl>
2022-06-07 15:59:55 +0200

    lp[p[l

--- Assets/_GAME/Scripts/Enemy/TurretEnemt.cs
+++ Assets/_GAME/Scripts/Enemy/TurretEnemt.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TurretEnemt : MonoBehaviour
 {
+
     [SerializeField] private float _radius = 10f;
     [SerializeField] private LayerMask _targetLayerMask;
     [SerializeField] private float _timeBetweenAttacks = 1;
@@ -12,6 +13,7 @@ public class TurretEnemt : MonoBehaviour
     [SerializeField] private GameObject _gunPoint;
     private void Update()
     {
+
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, _radius, _targetLayerMask);
         
         if (hitColliders.Length>0)
